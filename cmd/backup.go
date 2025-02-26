c90aec2ee178104fb28398360807a042dda1b5e8
Nouri Mohamed <mohamed.nouri.1997@gmail.com>
2025-02-26 20:35:13 +0100

    backup & started on sync

--- cmd/backup.go
+++ cmd/backup.go
@@ -12,6 +12,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var BACKUP_PATH = ".its_win_wine/backup"
+
 // CompareFolderModTimes compares two paths (files or folders) and returns the path of the most recently modified one,
 // along with its modification time and any error encountered.
 // If both paths have the same modification time, the first path is returned.
@@ -42,31 +44,68 @@ func CompareFolderModTimes(path1, path2 string) (string, time.Time, error) {
 	return path1, modTime1, nil
 }
 
+// backup a folder
+func BackupPath(path, name string) error {
+	err := os.MkdirAll(BACKUP_PATH, os.ModeDir)
+	if err != nil {
+		return err
+	}
+	newPath := BACKUP_PATH + "/" + name + "_" + time.Now().Format("2006_01_02_15_04")
+	os.CopyFS(newPath, os.DirFS(path))
+	fmt.Println("Created Backup for " + name + " at")
+	fmt.Println(newPath)
+
+	return nil
+}
+
 // backupCmd represents the backup command
 var backupCmd = &cobra.Command{
 	Use:   "backup",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `Create a backup for the linux and/or the windows path`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("backup called")
+		err := os.MkdirAll(BACKUP_PATH, os.ModeDir)
+		if err != nil {
+			println(err.Error())
+			os.Exit(1)
+		}
+		win, err := cmd.Flags().GetBool("windows")
+		if err != nil {
+			println(err.Error())
+			return
+		}
+		linux, err := cmd.Flags().GetBool("linux")
+		if err != nil {
+			println(err.Error())
+			return
+		}
+		data := ReadData()
+
+		for _, row := range data[1:] {
+			if win {
+				err = BackupPath(row[1], row[0]+"_win")
+			}
+			if linux {
+				err = BackupPath(row[2], row[0]+"_linux")
+			}
+			//    s, _, err := CompareFolderModTimes(row[1], row[2])
+			if err != nil {
+				println(err.Error())
+				os.Exit(1)
+			}
+			// if s == row[1] {
+			// 	fmt.Println("the newer file is win")
+			// }
+			// if s == row[2] {
+			// 	fmt.Println("the newer file is linux")
+			// }
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(backupCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// backupCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// backupCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	backupCmd.Flags().BoolP("windows", "w", false, "the windows path to sync")
+	backupCmd.Flags().BoolP("linux", "l", false, "the linux/wine path to sync")
 }
